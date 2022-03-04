dd576910d9842c1caac724f1a1b780f53d69722a
695996 <100864063+695996@users.noreply.github.com>
2022-03-04 09:48:01 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-
+let card_4: Sprite = null
+let card3: Sprite = null
+let card_2: Sprite = null
+let card_1: Sprite = null
+card_1.setPosition(20, 43)
+card_2.setPosition(51, 45)
+card3.setPosition(90, 49)
+card_4.setPosition(128, 54)
+card_1 = null
+card_2 = null
+card3 = null
+card_4 = null
